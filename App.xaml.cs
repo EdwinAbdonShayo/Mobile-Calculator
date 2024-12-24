59a07b999e559eb663200c8664aa49a8ef0b7ead
EdwinAbdonShayo <contact@edwinshayo.com>
2024-12-24 20:09:31 +0400

    Template for App Development
    Initialize NumiX MAUI project with multi-platform support

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NumiX
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NumiX
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
